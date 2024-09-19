c91749b47089227a89e81d218adc3607769aa665
yves <rroughpatch@gmail.com>
2024-09-19 16:13:56 -0500

    idk

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,7 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
+  trailingComma: "es5",
   plugins: ["prettier-plugin-tailwindcss"],
 };
 
-export default config;
+module.exports = config;
